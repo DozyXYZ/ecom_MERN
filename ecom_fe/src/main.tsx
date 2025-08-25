5dcb22100e957254bef53967b6e29defa1982777
Dozy <dotuan14747@gmail.com>
2025-08-25 14:52:39 +0300

    Implemented Router. Created Home and Product Details page

--- ecom_fe/src/main.tsx
+++ ecom_fe/src/main.tsx
@@ -3,9 +3,27 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.tsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./pages/Home.tsx";
+import ProductDetails from "./pages/ProductDetails.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<Home />} />
+      <Route path="product/:slug" element={<ProductDetails />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
