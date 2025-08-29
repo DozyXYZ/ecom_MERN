b90996c3d45d708bcd829b61ef3f92b98e7bcc46
Dozy <dotuan14747@gmail.com>
2025-08-29 07:35:54 +0300

    FE: Added convertProductToCartItem utility function

--- ecom_fe/src/utils.ts
+++ ecom_fe/src/utils.ts
@@ -1,7 +1,22 @@
 import type { ApiError } from "./types/ApiError";
+import type { ICartItem } from "./types/Cart";
+import type { IProduct } from "./types/Product";
 
 export const getError = (error: ApiError) => {
   return error.response && error.response.data.message
     ? error.response.data.message
     : error.message;
+};
+
+export const convertProductToCartItem = (product: IProduct): ICartItem => {
+  const cartItem: ICartItem = {
+    _id: product._id,
+    name: product.name,
+    slug: product.slug,
+    image: product.image,
+    price: product.price,
+    countInStock: product.countInStock,
+    quantity: 1,
+  };
+  return cartItem;
 };
