cd9151f0f947bc5687a743544b05556809d860d5
Dozy <dotuan14747@gmail.com>
2025-08-29 07:34:39 +0300

    FE: Implemented state management for cart and add item to cart reducer

--- ecom_fe/src/Store.tsx
+++ ecom_fe/src/Store.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import type { ICart, ICartItem } from "./types/Cart";
 
 type AppState = {
   mode: string;
+  cart: ICart;
 };
 
 const initialState: AppState = {
@@ -11,14 +13,47 @@ const initialState: AppState = {
       window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
     : "light",
+
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems")!)
+      : [],
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress")!)
+      : {},
+    paymentMethod: localStorage.getItem("paymentMethod")
+      ? localStorage.getItem("paymentMethod")!
+      : "PayPal",
+    itemsPrice: 0,
+    shippingPrice: 0,
+    taxPrice: 0,
+    totalPrice: 0,
+  },
 };
 
-type Action = { type: "SWITCH_MODE" };
+type Action =
+  | { type: "SWITCH_MODE" }
+  | { type: "CART_ADD_ITEM"; payload: ICartItem };
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
     case "SWITCH_MODE":
-      return { mode: state.mode === "dark" ? "light" : "dark" };
+      return { ...state, mode: state.mode === "dark" ? "light" : "dark" };
+
+    case "CART_ADD_ITEM": {
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item: ICartItem) => item._id === newItem._id
+      );
+      const CartItems = existItem
+        ? state.cart.cartItems.map((item: ICartItem) =>
+            item._id === existItem._id ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem];
+      localStorage.setItem("cartItems", JSON.stringify(CartItems));
+      return { ...state, cart: { ...state.cart, cartItems: CartItems } };
+    }
+
     default:
       return state;
   }
