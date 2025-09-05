ae822497158f3ab3c939ed082890a29b41371c22
Dozy <dotuan14747@gmail.com>
2025-09-05 10:14:41 +0300

    FE: Added route to PaymentMethodPage in main

--- ecom_fe/src/main.tsx
+++ ecom_fe/src/main.tsx
@@ -20,6 +20,7 @@ import CartPage from "./pages/CartPage.tsx";
 import SignInPage from "./pages/SignInPage.tsx";
 import SignUpPage from "./pages/SignUpPage.tsx";
 import ShippingAddressPage from "./pages/ShippingAddressPage.tsx";
+import PaymentMethodPage from "./pages/PaymentMethodPage.tsx";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:4000" : "/";
@@ -33,6 +34,7 @@ const router = createBrowserRouter(
       <Route path="signin" element={<SignInPage />} />
       <Route path="signup" element={<SignUpPage />} />
       <Route path="shipping" element={<ShippingAddressPage />} />
+      <Route path="payment" element={<PaymentMethodPage />} />
     </Route>
   )
 );
