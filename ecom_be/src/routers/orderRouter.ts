3a9a2bb735996795d63387542bc17dd3012bc17e
Dozy <dotuan14747@gmail.com>
2025-09-11 20:04:46 +0300

    BE: Created create order api

---
+++ ecom_be/src/routers/orderRouter.ts
@@ -0,0 +1,35 @@
+import { Router, Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+import { isAuth } from "../utils";
+import { Order, OrderModel } from "../models/orderModel";
+import { Product } from "../models/productModel";
+
+const orderRouter = Router();
+
+// POST /api/orders
+orderRouter.post(
+  "/",
+  isAuth,
+  asyncHandler(async (req: Request, res: Response) => {
+    if (req.body.orderItems.length === 0) {
+      res.status(400).json({ message: "Cart is empty" });
+    } else {
+      const createdOrder = await OrderModel.create({
+        orderItems: req.body.orderItems.map((x: Product) => ({
+          ...x,
+          product: x._id,
+        })),
+        shippingAddress: req.body.shippingAddress,
+        paymentMethod: req.body.paymentMethod,
+        itemsPrice: req.body.itemsPrice,
+        shippingPrice: req.body.shippingPrice,
+        taxPrice: req.body.taxPrice,
+        totalPrice: req.body.totalPrice,
+        user: req.user._id,
+      } as Order);
+      res.status(201).json({ message: "Order Created", order: createdOrder });
+    }
+  })
+);
+
+export default orderRouter;
