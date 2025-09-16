de80e64b9ae3ef87e0587c2e7ec1b7da2cef5981
Dozy <dotuan14747@gmail.com>
2025-09-16 16:49:41 +0300

    FE: Created useGetorderHistoryQuery hook

--- ecom_fe/src/hooks/orderHooks.ts
+++ ecom_fe/src/hooks/orderHooks.ts
@@ -53,3 +53,10 @@ export const usePayOrderMutation = () => {
     },
   });
 };
+
+export const useGetOrderHistoryQuery = () =>
+  useQuery({
+    queryKey: ["order-history"],
+    queryFn: async () =>
+      (await apiClient.get<Order[]>(`api/orders/history`)).data,
+  });
